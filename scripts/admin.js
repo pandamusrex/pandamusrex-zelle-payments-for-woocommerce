93d87e6eecf3bbe3fc5597b108f2baa8ac5d181a
pandamusrex <pandamusrex@gmail.com>
2025-08-10 15:56:32 -0700

    Tweak media instantiation a bit in js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,28 +1,28 @@
-console.log('pandamusrex admin.js loaded successfully');
+console.log( 'pandamusrex admin.js loaded successfully' );
 
-jQuery(document).ready(function($){
+jQuery( document ).ready( function( $ ){
     var custom_uploader;
-    $('#pandamusrex_zelle_qr_code_upload_button').click(function(e) {
+    $( '#pandamusrex_zelle_qr_code_upload_button' ).click( function( e ) {
         e.preventDefault();
-        //If the uploader object has already been created, reopen the dialog
-        if (custom_uploader) {
+        // If the uploader object has already been created, reopen the dialog
+        if ( custom_uploader ) {
             custom_uploader.open();
             return;
         }
-        //Extend the wp.media object
-        custom_uploader = wp.media.frames.file_frame = wp.media({
+        // Extend the wp.media object
+        custom_uploader = wp.media( {
             title: 'Choose Image',
             button: {
                 text: 'Choose Image'
             },
             multiple: false
-        });
-        //When a file is selected, grab the URL and set it as the text field's value
-        custom_uploader.on('select', function() {
-            attachment = custom_uploader.state().get('selection').first().toJSON();
-            $('#pandamusrex_zelle_qr_code_upload_text').val(attachment.url);
-        });
-        //Open the uploader dialog
+        } );
+        // When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on( 'select', function() {
+            attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
+            $('#pandamusrex_zelle_qr_code_upload_text').val( attachment.url );
+        } );
+        // Open the uploader dialog
         custom_uploader.open();
-    });
-});
+    } );
+} );
